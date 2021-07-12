db46621ee6c8f170ca893348664a13d0c9274f6f
Jervy Umandap <mac@Jervys-MacBook-Pro.local>
2021-07-12 19:23:14 +0800

    Basic calculator written in SwiftUI.

--- SwiftUI Calc/SwiftUI Calc/ContentView.swift
+++ SwiftUI Calc/SwiftUI Calc/ContentView.swift
@@ -40,9 +40,16 @@ enum CalcButton: String {
     }
 }
 
+enum Operation {
+    case add, subtract, multiply, divide, none
+    
+}
+
 struct ContentView: View {
     
     @State var value = "0"
+    @State var runningNumber = 0
+    @State var currentOperation: Operation = .none
     
     let buttons: [[CalcButton]] = [
         [.clear, .nega_posi, .percent, .divide],
@@ -91,12 +98,60 @@ struct ContentView: View {
     }
     
     func didTap(button: CalcButton) {
-        
+        switch button {
+        case .add, .subtract, .multiply, .divide, .equal:
+            if button == .add {
+                self.currentOperation = .add
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .subtract {
+                self.currentOperation = .subtract
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .multiply {
+                self.currentOperation = .multiply
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .divide {
+                self.currentOperation = .divide
+                self.runningNumber = Int(self.value) ?? 0
+            } else if button == .equal {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                
+                switch self.currentOperation {
+                case .add:
+                    self.value = "\(runningValue + currentValue)"
+                case .subtract:
+                    self.value = "\(runningValue - currentValue)"
+                case .multiply:
+                    self.value = "\(runningValue * currentValue)"
+                case .divide:
+                    self.value = "\(runningValue / currentValue)"
+                case .none:
+                    break
+                }
+            }
+            // set to zero when tap a math operation
+            if button != .equal {
+                self.value = "0"
+            }
+            
+        case .clear:
+            self.value = "0"
+        case .decimal, .nega_posi, .percent:
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                value = number
+            } else {
+                self.value = "\(self.value)\(number)"
+            }
+            
+        }
     }
     
     func buttonWidth(item: CalcButton) -> CGFloat {
         if item == .zero {
-            return ((UIScreen.main.bounds.width - (4*12)) / 4) * 2
+            return ((UIScreen.main.bounds.width - (4*12)) / 4) * 2.05
         }
         return (UIScreen.main.bounds.width - (5*12)) / 4
     }
