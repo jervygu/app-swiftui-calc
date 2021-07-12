5f5e67ad935d73dd77c2c0e71ec908f5cd7bf294
Jervy Umandap <mac@Jervys-MacBook-Pro.local>
2021-07-12 17:48:34 +0800

    Starter Project.

---
+++ SwiftUI Calc/SwiftUI Calc/SwiftUI_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CalcApp.swift
+//  SwiftUI Calc
+//
+//  Created by Jervy Umandap on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
